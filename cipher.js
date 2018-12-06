137a13b2cfcfec5afa3e0f1ec1770c0f9de3e9ab
Saaras225 <sarayance@gmail.com>
2018-12-06 19:57:22 -0300

    release 1 (archivos faltantes)

--- cipher.js
+++ cipher.js
@@ -1,8 +1,36 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (stringU,jump) => {
+      let stringResult="";
+      let code= stringU.toUpperCase();
+      for (i=0;i<=code.length;i++){
+        let carac= code.charAt(i);
+        let codeString= carac.charCodeAt(0);
+        if (codeString>=65&&codeString<=90){
+          let ascii= (codeString-65+jump)%26+65;
+          let coding= String.fromCharCode(ascii);
+          stringResult= stringResult+coding;
+        } else {
+          stringResult= stringResult+carac;
+        }
+      }
+      return stringResult;
   },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+
+    decode: (stringU,jump) => {
+      let stringResult="";
+      let code= stringU.toUpperCase();
+      for (i=0;i<=code.length;i++){
+        let carac= code.charAt(i);
+        let codeString= carac.charCodeAt(0);
+        if (codeString>=65&&codeString<=90){
+          let ascii= (codeString+65-jump)%26+65;
+          let coding= String.fromCharCode(ascii);
+          stringResult= stringResult+coding;
+        } else {
+          stringResult= stringResult+carac;
+        }
+      }
+      return stringResult;
+    }
+
+  };
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-/* Acá va tu código */
+function decodifica(){
+  var texto= document.getElementById("codemsj").value;
+  var salto= parseInt(document.getElementById("number").value);
+  var cifrado= window.cipher.decode(texto, salto);
+  document.getElementById("decodemsj").value=cifrado;
+  }
+
+  function codificar(){
+    var texto= document.getElementById("codemsj").value;
+    var salto= parseInt(document.getElementById("number").value);
+    var cifrado= window.cipher.encode(texto, salto);
+    document.getElementById("decodemsj").value=cifrado;
+
+  }
+
+  function reset(){
+    document.getElementById("codemsj").value="";
+    document.getElementById("number").value="";
+    document.getElementById("decodemsj").value="";
+  }
+  
+
+document.getElementById("code").addEventListener("click", codificar);
+document.getElementById("decode").addEventListener("click", decodifica);
